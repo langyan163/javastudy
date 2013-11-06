67d71a1e3c83c618cf061d9963dac4537418a44c
langyan163 <langyan.lin@163.com>
2013-11-06 23:18:02 +0800

    commit

---
+++ src/main/java/com/michael/study/App.java
@@ -0,0 +1,13 @@
+package com.michael.study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
